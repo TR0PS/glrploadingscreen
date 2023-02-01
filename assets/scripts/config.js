4a3ab94a940589796073dca67ad2c583881e5539
TR0PS <68011912+TR0PS@users.noreply.github.com>
2023-02-01 20:06:25 +0100

    Add files via upload

---
+++ assets/scripts/config.js
@@ -0,0 +1,30 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "snxwyyy.test";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "Klick um dem Offiziellen Discord zu joinen!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/wRzn9shc3j"},
+    {name: "instagram", label: "Instagram", description: "Hier kann dein Server-Instagram Account verlinkt werden!", icon: "assets/media/icons/tiktok.png", link: "#"},
+    {name: "tebex", label: "Donations", description: "Über Spenden freuen wir uns! melde dich im Ticket-Tool falls du Spenden willst :)", icon: "assets/media/icons/tebex.png", link: "https://discord.gg/wRzn9shc3j"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "snxwyyy", description: "Owner & Dev", color: "#ff0000", image: "https://media.discordapp.net/attachments/974294148641357855/1033505890294321162/4640bd15274031.562bcf17bb80f.png"},
+    {name: "FlyDaniHD", description: "Owner", color: "#ff0000", image: "https://media.discordapp.net/attachments/974294148641357855/1033505890294321162/4640bd15274031.562bcf17bb80f.png"},
+
+
+];
+
+// Categories
+Config.Categories = [
+    {label: "Socialmedia", default: true},
+    {label: "Team", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
