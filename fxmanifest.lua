d43abd5cd191636538141df6d388e35c244c57d8
TR0PS <68011912+TR0PS@users.noreply.github.com>
2023-02-01 20:05:46 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'snxwyyy#0130' -- # Discord - https://discord.gg/wRzn9shc3j
+description 'snx-loadingscreen'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+client_script 'client.lua'
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
